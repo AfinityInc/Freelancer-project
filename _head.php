bd74d4827339d1c7e769ccc013b429f2c68f2fe1
Arslan Malik <arslanamjad85@gmail.com>
2018-10-10 15:59:19 +0500

    file added
    added a file

---
+++ _head.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+	<head>
+		<title>
+			ACCP | Homepage
+		</title>
+		<meta>
+		<link type="text/css" rel="stylesheet" href="includes/css/style.css">
+		<script></script>
+	</head>
+	<body>
+		
